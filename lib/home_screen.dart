2be82a73661fe2627b31913dc18da5c3910a3127
divyang-mobio <107481877+divyang-mobio@users.noreply.github.com>
2022-09-02 15:15:17 +0530

    razor pay

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:multi_select_flutter/bottom_sheet/multi_select_bottom_sheet_field.dart';
 import 'package:multi_select_flutter/chip_display/multi_select_chip_display.dart';
 import 'package:multi_select_flutter/chip_field/multi_select_chip_field.dart';
@@ -22,11 +23,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final razorpay = Razorpay();
-  // List _selectedAnimals2 = [];
-  // List _selectedAnimals3 =[];
-  // final _items = _animals
-  //     .map((animal) => MultiSelectItem<Animal>(animal, animal.name))
-  //     .toList();
+  List _selectedAnimals2 = [];
+  List _selectedAnimals3 =[];
+  final _items = _animals
+      .map((animal) => MultiSelectItem<Animal>(animal, animal.name))
+      .toList();
 
   void handlerPaymentSuccess(PaymentSuccessResponse response) =>
       showToast("Payment success", position: ToastPosition.bottom);
@@ -58,12 +59,24 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  stopTakingSS() async {
+    await FlutterWindowManager.addFlags(
+        FlutterWindowManager.FLAG_SECURE);
+  }
+
   @override
   void initState() {
     super.initState();
     razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
     razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
     razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+    stopTakingSS();
+  }
+
+  @override
+  void dispose() {
+    razorpay.clear();
+    super.dispose();
   }
 
   @override
@@ -75,50 +88,50 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              // MultiSelectBottomSheetField(
-              //   initialChildSize: 0.4,
-              //   listType: MultiSelectListType.CHIP,
-              //   searchable: true,
-              //   buttonText: const Text("Favorite Animals"),
-              //   title: const Text("Animals"),
-              //   items: _items,
-              //   onConfirm: (values) {
-              //     _selectedAnimals2 = values;
-              //     setState(() {});
-              //   },
-              //   chipDisplay: MultiSelectChipDisplay(
-              //     onTap: (value) {
-              //       setState(() {
-              //         _selectedAnimals2.remove(value);
-              //       });
-              //     },
-              //   ),
-              // ),
-              // _selectedAnimals2 == null || _selectedAnimals2.isEmpty
-              //     ? Container(
-              //         padding: const EdgeInsets.all(10),
-              //         alignment: Alignment.centerLeft,
-              //         child: const Text(
-              //           "None selected",
-              //           style: TextStyle(color: Colors.black54),
-              //         ))
-              //     : Container(),
-              // const SizedBox(height: 40),
-              // MultiSelectChipField(
-              //   items: _items,
-              //   initialValue: _selectedAnimals3,
-              //   title: const Text("Animals"),
-              //   headerColor: Colors.blue.withOpacity(0.5),
-              //   decoration: BoxDecoration(
-              //     border: Border.all(color: Colors.blue, width: 1.8),
-              //   ),
-              //   selectedChipColor: Colors.blue.withOpacity(0.5),
-              //   selectedTextStyle: TextStyle(color: Colors.blue[800]),
-              //   onTap: (values) {
-              //     _selectedAnimals3 = values;
-              //     setState(() {});
-              //   },
-              // ),
+              MultiSelectBottomSheetField(
+                initialChildSize: 0.4,
+                listType: MultiSelectListType.CHIP,
+                searchable: true,
+                buttonText: const Text("Favorite Animals"),
+                title: const Text("Animals"),
+                items: _items,
+                onConfirm: (values) {
+                  _selectedAnimals2 = values;
+                  setState(() {});
+                },
+                chipDisplay: MultiSelectChipDisplay(
+                  onTap: (value) {
+                    setState(() {
+                      _selectedAnimals2.remove(value);
+                    });
+                  },
+                ),
+              ),
+              _selectedAnimals2 == null || _selectedAnimals2.isEmpty
+                  ? Container(
+                      padding: const EdgeInsets.all(10),
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "None selected",
+                        style: TextStyle(color: Colors.black54),
+                      ))
+                  : Container(),
+              const SizedBox(height: 40),
+              MultiSelectChipField(
+                items: _items,
+                initialValue: _selectedAnimals3,
+                title: const Text("Animals"),
+                headerColor: Colors.blue.withOpacity(0.5),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 1.8),
+                ),
+                selectedChipColor: Colors.blue.withOpacity(0.5),
+                selectedTextStyle: TextStyle(color: Colors.blue[800]),
+                onTap: (values) {
+                  _selectedAnimals3 = values;
+                  setState(() {});
+                },
+              ),
               MaterialButton(
                   color: Colors.blue,
                   onPressed: openCheckout,
@@ -131,32 +144,32 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-// List<Animal> _animals = [
-//   Animal(id: 1, name: "Lion"),
-//   Animal(id: 2, name: "Flamingo"),
-//   Animal(id: 3, name: "Hippo"),
-//   Animal(id: 4, name: "Horse"),
-//   Animal(id: 5, name: "Tiger"),
-//   Animal(id: 6, name: "Penguin"),
-//   Animal(id: 7, name: "Spider"),
-//   Animal(id: 8, name: "Snake"),
-//   Animal(id: 9, name: "Bear"),
-//   Animal(id: 10, name: "Beaver"),
-//   Animal(id: 11, name: "Cat"),
-//   Animal(id: 12, name: "Fish"),
-//   Animal(id: 13, name: "Rabbit"),
-//   Animal(id: 14, name: "Mouse"),
-//   Animal(id: 15, name: "Dog"),
-//   Animal(id: 16, name: "Zebra"),
-//   Animal(id: 17, name: "Cow"),
-//   Animal(id: 18, name: "Frog"),
-//   Animal(id: 19, name: "Blue Jay"),
-//   Animal(id: 20, name: "Moose"),
-//   Animal(id: 21, name: "Gecko"),
-//   Animal(id: 22, name: "Kangaroo"),
-//   Animal(id: 23, name: "Shark"),
-//   Animal(id: 24, name: "Crocodile"),
-//   Animal(id: 25, name: "Owl"),
-//   Animal(id: 26, name: "Dragonfly"),
-//   Animal(id: 27, name: "Dolphin")
-// ];
+List<Animal> _animals = [
+  Animal(id: 1, name: "Lion"),
+  Animal(id: 2, name: "Flamingo"),
+  Animal(id: 3, name: "Hippo"),
+  Animal(id: 4, name: "Horse"),
+  Animal(id: 5, name: "Tiger"),
+  Animal(id: 6, name: "Penguin"),
+  Animal(id: 7, name: "Spider"),
+  Animal(id: 8, name: "Snake"),
+  Animal(id: 9, name: "Bear"),
+  Animal(id: 10, name: "Beaver"),
+  Animal(id: 11, name: "Cat"),
+  Animal(id: 12, name: "Fish"),
+  Animal(id: 13, name: "Rabbit"),
+  Animal(id: 14, name: "Mouse"),
+  Animal(id: 15, name: "Dog"),
+  Animal(id: 16, name: "Zebra"),
+  Animal(id: 17, name: "Cow"),
+  Animal(id: 18, name: "Frog"),
+  Animal(id: 19, name: "Blue Jay"),
+  Animal(id: 20, name: "Moose"),
+  Animal(id: 21, name: "Gecko"),
+  Animal(id: 22, name: "Kangaroo"),
+  Animal(id: 23, name: "Shark"),
+  Animal(id: 24, name: "Crocodile"),
+  Animal(id: 25, name: "Owl"),
+  Animal(id: 26, name: "Dragonfly"),
+  Animal(id: 27, name: "Dolphin")
+];
